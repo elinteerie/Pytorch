81bb62e6a5ac988ebf17597a2023e67219ba9fae
Elint Eerie <96844092+elinteerie@users.noreply.github.com>
2022-09-28 11:23:06 +0100

    Update stepbystep.py

--- stepbystep.py
+++ stepbystep.py
@@ -131,9 +131,14 @@ class StepByStep(object):
 
     def set_seed(self, seed=42):
         torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False    
+        torch.backends.cudnn.benchmark = False
         torch.manual_seed(seed)
         np.random.seed(seed)
+        random.seed(seed)
+        try:
+            self.train_loader.sampler.generator.manual_seed(seed)
+        except AttributeError:
+        pass
     
     def train(self, n_epochs, seed=42):
         # To ensure reproducibility of the training process
